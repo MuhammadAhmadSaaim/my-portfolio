8538a01d29a42965353cda2b58acc6df14930923
MuhammadAhmadSaaim <ahmadsaaim2002@gmail.com>
2024-05-10 12:33:30 +0500

    Made Changes to Projects

--- app/Components/Projects.tsx
+++ app/Components/Projects.tsx
@@ -35,7 +35,7 @@ const Projects = () => {
                 </div>
                 <div data-aos="fade-up" data-aos-delay="1500">
                     <div className="transform cursor-pointer hover:translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
-                        <Image src="/images/p2.jpg" alt="portfolio" layout="fill" className="object-contain" />
+                        <Image src="/images/p5.jpg" alt="portfolio" layout="fill" className="object-contain" />
                     </div>
                 </div>
             </div>
