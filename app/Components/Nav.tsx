12c7a630d5c5e1e8f44c5ae45350a167cb202ffd
MuhammadAhmadSaaim <ahmadsaaim2002@gmail.com>
2024-05-10 11:54:08 +0500

    Text Changes

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -10,8 +10,8 @@ const Nav = ({ openNav }: Props) => {
     <div className="w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#141c27] shadow-md">
       <div className="flex items-center justify-between w-[90%] mx-auto h-[100%]">
         <h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
-          WEB
-          <span className="text-purple-900">DEV</span>
+          PORT
+          <span className="text-purple-900">FOLIO</span>
         </h1>
         <div className="nav-link">HOME</div>
         <div className="nav-link">SERVICES</div>
