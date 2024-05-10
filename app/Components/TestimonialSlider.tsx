4908b01926c37e2d952c5a9748a71ba35b249da5
MuhammadAhmadSaaim <ahmadsaaim2002@gmail.com>
2024-05-10 11:34:27 +0500

    Updated Review

--- app/Components/TestimonialSlider.tsx
+++ app/Components/TestimonialSlider.tsx
@@ -35,7 +35,7 @@ const TestimonialSlider = () => {
             itemClass="item"
         >
             <ClientReview image="/images/c1.jpg" name="Ali Nawaz" role="MERN Stack Developer" body="His attention to detail and problem-solving skills have truly set him apart." rating={5} />
-            <ClientReview image="/images/c2.jpg" name="Fahad Iqbal" role="AI Specilist" body="As an AI Specialist has been invaluable to our project. His deep understanding of machine learning algorithms and artificial intelligence has significantly enhanced the functionality of our application." rating={4} />
+            <ClientReview image="/images/c2.jpg" name="Fahad Iqbal" role="AI Specilist" body="As an AI Specialist, Ahmad has been invaluable to our project. His deep understanding of machine learning algorithms and artificial intelligence has significantly enhanced the functionality of our application." rating={4} />
             <ClientReview image="/images/c3.jpg" name="Reyan Ahtesham" role="UI/UX Designer" body="His designs are not only visually stunning but also highly intuitive and user-friendly." rating={5} />
             <ClientReview image="/images/c4.jpg" name="Saba Shafique" role="Web Developer" body="Ahmad has been instrumental Web Developer in our project. His expertise in front-end and back-end development has resulted in a seamless and responsive website." rating={5} />
         </Carousel>
