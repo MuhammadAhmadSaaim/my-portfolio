e65ae7eb5b930a2c1c656e2c4a0ac2f9409bb007
Ahmad <131032658+MuhammadAhmadSaaim@users.noreply.github.com>
2024-07-30 12:28:57 +0500

    updated blogs link

--- app/Components/Nav.tsx
+++ app/Components/Nav.tsx
@@ -6,6 +6,11 @@ interface Props {
 }
 
 const Nav = ({ openNav }: Props) => {
+
+  const goToProjectLink = (link: string) => {
+    window.open(link);
+};
+
   return (
     <div className="w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#141c27] shadow-md">
       <div className="flex items-center justify-between w-[90%] mx-auto h-[100%]">
@@ -15,7 +20,7 @@ const Nav = ({ openNav }: Props) => {
         </h1>
         <div className="nav-link">HOME</div>
         <div className="nav-link">SERVICES</div>
-        <div className="nav-link">BLOGS</div>
+        <div onClick={() => goToProjectLink('https://medium.com/@muhammadahmadsaaim')} className="nav-link">BLOGS</div>
         <div className="nav-link">CONTACT</div>
         <div onClick={openNav}>
           <Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-purple-900" />
