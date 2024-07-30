271136bcd8cef6c9baab6e813a5564c48ca7b3eb
Ahmad <131032658+MuhammadAhmadSaaim@users.noreply.github.com>
2024-07-30 12:40:14 +0500

    removed go to video

--- app/Components/Hero.tsx
+++ app/Components/Hero.tsx
@@ -1,16 +1,13 @@
-import React, { useState } from 'react';
-import Particle from "./Particles";
-import TextEffect from "./TextEffects";
+import React from 'react'
+import Particle from "./Particles"
+import TextEffect from "./TextEffects"
 import Image from "next/legacy/image";
-import { ArrowDownTrayIcon } from "@heroicons/react/16/solid";
-import Modal from 'react-modal';
+import { ArrowDownTrayIcon, PlayCircleIcon } from "@heroicons/react/16/solid";
 
 const greeting = "HI I'M";
 const PDF_URL = '/docs/MuhammadAhmadSaaim-CV.pdf';
 
 const Hero = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
     const handleDownloadCV = () => {
         const downloadLink = document.createElement('a');
         downloadLink.href = PDF_URL;
@@ -18,17 +15,9 @@ const Hero = () => {
         downloadLink.click();
     };
 
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
-
     return (
         <div className="h-[88vh] bg-[url('/images/banner.jpg')] mt-[10vh] bg-cover bg-center">
-            <div><Particle /></div>
+            <div ><Particle /></div>
             <div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
                 <div>
                     <h1 className="text-[35px] md:text-[50px] text-white font-bold">
@@ -43,30 +32,14 @@ const Hero = () => {
                             <p>Download CV</p>
                             <ArrowDownTrayIcon className="w-[1.6rem] h-[1.7rem] text-black" />
                         </button>
-                        <button onClick={openModal} className="flex items-center space-x-2">
-                            <p className="text-[20px] font-semibold text-white">View CV</p>
-                        </button>
                     </div>
                 </div>
                 <div className="w-[500px] hidden bg-[#55e6a5] relative lg:flex items-center rounded-full h-[500px]">
                     <Image src="/images/u1.jpg" alt="user" layout="fill" className="object-cover rounded-full" />
                 </div>
             </div>
-
-            <Modal
-                isOpen={isModalOpen}
-                onRequestClose={closeModal}
-                contentLabel="CV Modal"
-                className="modal"
-                overlayClassName="modal-overlay"
-            >
-                <div className="relative w-full h-full">
-                    <button onClick={closeModal} className="absolute top-2 right-2 text-white">X</button>
-                    <iframe src={PDF_URL} className="w-full h-full" frameBorder="0"></iframe>
-                </div>
-            </Modal>
         </div>
     )
 }
 
-export default Hero;
+export default Hero
